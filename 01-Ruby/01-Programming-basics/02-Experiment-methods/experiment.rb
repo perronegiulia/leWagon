2283972e1a78d795663a8298be6ceb852a42ca17
Giulia Perrone <emaildagiuliaperrone@gmail.com>
2025-10-18 00:46:46 -0300

    experiment

---
+++ 01-Ruby/01-Programming-basics/02-Experiment-methods/experiment.rb
@@ -0,0 +1,35 @@
+# Warning:
+# - One line of code for each method
+# - Just look in the doc for the right method of the String, Integer, and Array classes!
+
+def get_rid_of_surrounding_whitespaces(a_string)
+  a_string.strip
+end
+
+def belongs_to?(a_string, a_word)
+  a_string.include?(a_word)
+end
+
+def replace(initial_string, old_letter, new_letter)
+  initial_string.gsub(old_letter, new_letter)
+end
+
+def exactly_divide(an_integer, a_divider)
+  an_integer.fdiv(a_divider)
+end
+
+def divisible_by_two?(an_integer)
+  an_integer.even?
+end
+
+def random_subset(an_array, sample_size)
+  an_array.sample(sample_size)
+end 
+
+def randomize(an_array)
+  an_array.shuffle
+end
+
+def ascending_order(an_array)
+  an_array.sort
+end

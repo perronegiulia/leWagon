4f17d54965fdb33c6c4dac28ab0cf14f2c036a41
Giulia Perrone <emaildagiuliaperrone@gmail.com>
2025-10-19 04:57:51 -0300

    your age in days

---
+++ 01-Ruby/01-Programming-basics/03-Age-in-days/age_in_days.rb
@@ -0,0 +1,12 @@
+# This "require" line loads the contents of the "date" file from the standard
+# Ruby library, giving you access to the Date class.
+require "date"
+
+def age_in_days(day, month, year)
+  today = Date.today
+  birth = Date.new(year, month, day)
+
+  age = (today - birth).to_i
+  return age
+
+end

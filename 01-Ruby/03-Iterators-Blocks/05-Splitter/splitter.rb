6e76a91d9486516937f43590a3b7317f0a4dc277
Giulia Perrone <emaildagiuliaperrone@gmail.com>
2025-10-26 00:42:29 -0300

    splitting

---
+++ 01-Ruby/03-Iterators-Blocks/05-Splitter/splitter.rb
@@ -0,0 +1,15 @@
+def size_splitter(array, size)
+  # TODO: Return an array of two arrays, the first containing
+  #       words of length `size`, the second containing all the other words
+  #       In addition to this split, each array should be *sorted*
+  sorted = array.sort
+  splitted = sorted.partition {|element| element.size == size}
+end
+
+def block_splitter(array)
+  # TODO: Return an array of two arrays, the first containing
+  #       elements for which the given block yields true,
+  #       the second containing all the other elements.
+  #       No sort needed this time.
+  array.partition { |element| yield(element) }
+end

3888fa915cebfab72455864dcdc61cb5689becaa
Giulia Perrone <emaildagiuliaperrone@gmail.com>
2025-10-26 14:52:22 -0300

    Hash

---
+++ 01-Ruby/04-Hash-Symbols/restaurant_order.rb
@@ -0,0 +1,14 @@
+def poor_calories_counter(burger, side, beverage)
+  # TODO: return number of calories for this restaurant order
+  menu = {  
+  "Hamburger" => 250,
+  "Cheese Burger" => 300,
+  "Veggie Burger" => 540,
+  "Vegan Burger" => 350,
+  "Sweet Potatoes" => 230,
+  "Salad" => 15,
+  "Iced Tea" => 70,
+  "Lemonade" => 90
+}
+  menu[burger] + menu[side] + menu[beverage]
+end

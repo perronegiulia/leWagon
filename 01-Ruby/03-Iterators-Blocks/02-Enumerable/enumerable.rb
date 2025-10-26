651a5a31b1bb4f54dd5a56ee97d6f1c4015b2645
Giulia Perrone <emaildagiuliaperrone@gmail.com>
2025-10-26 01:00:40 -0300

    ajusting

--- 01-Ruby/03-Iterators-Blocks/02-Enumerable/enumerable.rb
+++ 01-Ruby/03-Iterators-Blocks/02-Enumerable/enumerable.rb
@@ -2,31 +2,31 @@ def sum_odd_indexed(array)
   i = 0
   sum = 0
 
-  array.each do |elemment|
-      sum += elemment if i.odd?
+  array.each do |element|
+      sum += element if i.odd?
       i += 1
   end
   return sum
 end
 
 def even_numbers(array)
-  array.select {|elemment| elemment.even?}
+  array.select {|element| element.even?}
 end
 
 def short_words(array, max_length)
-  array.reject {|elemment| elemment.length > max_length}
+  array.reject {|element| element.length > max_length}
 end
 
 def first_under(array, limit)
-  array.find {|elemment| elemment < limit}
+  array.find {|element| element < limit}
 end
 
 def add_bang(array)
-  array.map {|elemment| elemment << "!"}
+  array.map {|element| element << "!"}
 end
 
 def concatenate(array)
-  array.reduce {|all, current_elemment| all + current_elemment}
+  array.reduce {|all, current_element| all + current_element}
 end
 
 def sorted_pairs(array)

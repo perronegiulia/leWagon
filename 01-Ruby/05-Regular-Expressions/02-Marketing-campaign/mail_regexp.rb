fdcbae699701dabb0fcc7445d566253a978a4c7e
Giulia Perrone <perroneg@Mac-mini-de-Giulia.local>
2025-11-06 00:42:54 -0300

    regex, case

--- 01-Ruby/05-Regular-Expressions/02-Marketing-campaign/mail_regexp.rb
+++ 01-Ruby/05-Regular-Expressions/02-Marketing-campaign/mail_regexp.rb
@@ -12,14 +12,52 @@ end
 
 def group_by_tld(emails)
   # TODO: return a Hash with emails grouped by TLD
-  
+  emails.group_by do |email|
+    email.split('.').last
+  end
 end
 
 def compose_mail(email)
   # TODO: return a Hash with username, domain and tld extracted from email
+  username, domain_part = email.split('@')
+  domain_tld = domain_part.split('.')
+
+  {
+    username: username,
+    domain: domain_tld.first,
+    tld: domain_tld.last
+  }  
 end
 
 def compose_translated_email(email)
   # TODO: return a Hash with username, domain and tld extracted from email
+  parts = compose_mail(email) 
+  
   # TODO: translate subject, body, closing and signature, according to TLD
+
+  translated = {
+          body: "Come and visit us!",
+          closing: "See you soon",
+          signature: "The Team",
+          subject: "Our website is online",
+        }
+  
+  case parts [:tld]
+  when "de"
+    translated = {
+      subject: "Unsere Website ist jetzt online",
+      body: "Komm und besuche uns!",
+      closing: "Bis bald",
+      signature: "Das Team"
+    }
+  when "fr"
+      translated = {
+      body: "Venez nous rendre visite !",
+      closing: "A bientot",
+      signature: "L'équipe",
+      subject: "Notre site est en ligne",
+    }
+  end
+  parts.merge(translated)
+
 end
